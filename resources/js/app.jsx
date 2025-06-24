a98b47ff33cc2e18b7354938666c820f4e890e54
Muigaihacks <146855637+Muigaihacks@users.noreply.github.com>
2025-06-24 12:27:51 +0300

    Fix ESM error for laravel-vite-plugin and test Tailwind

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Login from './components/Auth/Login';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<Login />);
+root.render(
+    <div className="bg-blue-500 text-white p-4">
+        Testing Tailwind CSS
+    </div>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import react from 'vite-plugin-react';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.jsx'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
