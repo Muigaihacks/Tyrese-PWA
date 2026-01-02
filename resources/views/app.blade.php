059d8631def40886d78cc5b560446fd5ca33c630
Muigaihacks <146855637+Muigaihacks@users.noreply.github.com>
2026-01-02 12:19:45 +0300

    fix: Add CSRF token and noscript tag to React app view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <title>My App</title>
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
     <div id="root"></div>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
     
     <!-- Admin Auto-Refresh Script - Only runs on admin panel -->
     <script>
